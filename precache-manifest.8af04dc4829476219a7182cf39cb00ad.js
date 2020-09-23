e78c0dca21f04fdace431d3594d7d95889f136c4
Neha Bharati <nehabharati@Nehas-MacBook-Pro.local>
2020-09-23 16:47:45 +0530

    Updates

---
+++ precache-manifest.8af04dc4829476219a7182cf39cb00ad.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c1021811097daefdac73f8de4d673e",
+    "url": "/cinemeye/index.html"
+  },
+  {
+    "revision": "8a74ac07d88ce94205bf",
+    "url": "/cinemeye/static/css/main.89946305.chunk.css"
+  },
+  {
+    "revision": "a9db603945e862fd9121",
+    "url": "/cinemeye/static/js/2.09740e34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cinemeye/static/js/2.09740e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a74ac07d88ce94205bf",
+    "url": "/cinemeye/static/js/main.6a70bccd.chunk.js"
+  },
+  {
+    "revision": "5a6ee90f0ec176a3de21",
+    "url": "/cinemeye/static/js/runtime-main.b80ccd81.js"
+  },
+  {
+    "revision": "41d6dd46920f81567f9caec0efa870a1",
+    "url": "/cinemeye/static/media/21-bridges.41d6dd46.jpg"
+  },
+  {
+    "revision": "1632dcae59a030007cb0e74788775aeb",
+    "url": "/cinemeye/static/media/Edge-of-Tomorrow-Wallpapers-1.1632dcae.jpg"
+  },
+  {
+    "revision": "189e324b1b4deb792b85b6df6ee6c8a4",
+    "url": "/cinemeye/static/media/app-store-badge.189e324b.svg"
+  },
+  {
+    "revision": "57d27bb4e0501829e28ede1ba0d7a922",
+    "url": "/cinemeye/static/media/dolittle.57d27bb4.jpg"
+  },
+  {
+    "revision": "178c33f3f611ba04398e02f07895ee11",
+    "url": "/cinemeye/static/media/ep-1.178c33f3.jpg"
+  },
+  {
+    "revision": "c76ca01d507a667e0600ee9867138267",
+    "url": "/cinemeye/static/media/ep-2.c76ca01d.jpg"
+  },
+  {
+    "revision": "11cca91bb8b7594e702db8b7b5f50cce",
+    "url": "/cinemeye/static/media/ep-3.11cca91b.jpg"
+  },
+  {
+    "revision": "6067074dae1728030da68783403b60b4",
+    "url": "/cinemeye/static/media/ep-4.6067074d.jpg"
+  },
+  {
+    "revision": "f67c49565122953bfc5161272fc0f781",
+    "url": "/cinemeye/static/media/gentlemen2020_feature.f67c4956.jpg"
+  },
+  {
+    "revision": "b775eda8f6031baaad8721debd4a00aa",
+    "url": "/cinemeye/static/media/google-play-badge.b775eda8.svg"
+  },
+  {
+    "revision": "e082ca4f803b9b32376885fad70cdb42",
+    "url": "/cinemeye/static/media/interstellar_1.e082ca4f.svg"
+  },
+  {
+    "revision": "e1524873e0a426a02d6341867ebcb55a",
+    "url": "/cinemeye/static/media/joker.e1524873.jpg"
+  },
+  {
+    "revision": "403d09fc6b8f9114eb63214cede7d27e",
+    "url": "/cinemeye/static/media/jumanji.403d09fc.jpg"
+  },
+  {
+    "revision": "de80a18ddab76f38b321801b574a9b0b",
+    "url": "/cinemeye/static/media/logo.de80a18d.svg"
+  },
+  {
+    "revision": "6bcb11d47b3235fe84e35c15eb37f5ae",
+    "url": "/cinemeye/static/media/midway.6bcb11d4.jpg"
+  },
+  {
+    "revision": "fdc3d82d8db972847ab89f9ecb8d8203",
+    "url": "/cinemeye/static/media/mitty.fdc3d82d.jpg"
+  },
+  {
+    "revision": "8705c826f8661cfde64efa3842076a2f",
+    "url": "/cinemeye/static/media/westworld-1.8705c826.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cinemeye-design/precache-manifest.6f751251a9440eca0cbfa84de7707164.js"
+  "/cinemeye/precache-manifest.8af04dc4829476219a7182cf39cb00ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cinemeye-design/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cinemeye/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
